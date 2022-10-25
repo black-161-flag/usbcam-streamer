6a96eb810ffc47185c06e54a0fd9f99032de3eb0
black-161-flag <github@blackbirds.xyz>
2022-10-25 20:26:29 +0200

    Remove numpy version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='usbcam-streamer',
-    version='0.0.2',
+    version='0.0.3',
     packages=['usbcam-streamer'],
     scripts=['bin/usbcam-streamer'],
     description='simple mpg streamer for usbcameras',
     long_description=open('README.md').read(),
-    install_requires=["opencv-python", "simplejpeg", 'numpy <= 1.23.4'],
+    install_requires=["opencv-python", "simplejpeg", 'numpy'],
     package_dir={'usbcam-streamer': 'bin'},
 
 )
